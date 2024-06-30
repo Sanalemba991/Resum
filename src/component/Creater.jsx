2a9854651694b3c748a440b1f34cdb552e8c40e3
Sanalemba991 <154108599+Sanalemba991@users.noreply.github.com>
2024-06-30 10:45:47 +0530

    newly added photo

--- src/component/Creater.jsx
+++ src/component/Creater.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from 'react';
+import Navbar from './Navbar'; // Assuming './Navbar' is the correct path to your Navbar component
+import saImage from './sa.jpg'; // Import the image using relative path
 
 function Creater() {
   return (
     <div>
-         <Navbar></Navbar>
-    <div className='imK'>
-        <img src='https://scontent.fshl1-1.fna.fbcdn.net/v/t39.30808-6/439587916_3666898806915085_5574554955694853526_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=kUWAyRkrCLUQ7kNvgH5v4OH&_nc_ht=scontent.fshl1-1.fna&oh=00_AfCuoKnH4K9nEZTAek69nHOsGshJB6gvtAfaOXuhfK4dKg&oe=66424682'></img>
-      
-      <h1 className='Cte'>Created by</h1><h2 className='Samn'>
-        Laitonjam Sanalemba Meitei(MCA)
-      </h2>
-      <h2 className='Nie'>NIELIT IMPHAL</h2>
-    </div></div>
-  )
+      <Navbar />
+      <div className='imK'>
+        <img src={saImage} alt='Creator' /> {/* Use the imported image */}
+        <h1 className='Cte'>Created by</h1>
+        <h2 className='Samn'>Laitonjam Sanalemba Meitei (MCA)</h2>
+        <h2 className='Nie'>NIELIT IMPHAL</h2>
+      </div>
+    </div>
+  );
 }
 
-export default Creater
+export default Creater;
